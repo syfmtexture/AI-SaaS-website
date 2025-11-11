ebf056afd233071665dad1b9449125762f98f658
yoriiISalive <abhirajmukadam82@gmail.com>
2025-11-11 22:08:52 +0530

    started doing routing

--- saas-pro/src/App.jsx
+++ saas-pro/src/App.jsx
@@ -1,10 +1,20 @@
 import { useState } from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home.jsx'
 
 function App() {
 
   return (
     <>
-      
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path='/ai' element={<Layout />}>
+          
+          </Route>
+
+        </Routes>
+      </div>
     </>
   )
 }
